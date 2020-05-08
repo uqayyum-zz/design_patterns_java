2f6923925937da7ab2fd91951bca0fbd7dd1ce20
Umair Qayyum <umairqayyumk@gmail.com>
2020-05-08 18:45:08 +0500

    Kick off

---
+++ src/com/uqayyum/Main.java
@@ -0,0 +1,13 @@
+package com.uqayyum;
+
+/**
+ * @author UMAIR QAYYUM
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Kickoff");
+    }
+}
